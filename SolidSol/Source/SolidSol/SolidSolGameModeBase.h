e2386a0c466b404ad068d815a3db1ead6522f0b2
DESKTOP-ELQUVSM\Jerms <jeremycbrothers@gmail.com>
2023-03-01 09:20:26 -0600

    Initial commit

---
+++ SolidSol/Source/SolidSol/SolidSolGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SolidSolGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOLIDSOL_API ASolidSolGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
